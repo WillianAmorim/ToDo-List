91e41c072e73f55a7238a28304047cd2e5c0da0f
Willian <willian.amorim1@outlook.com>
2023-01-14 20:12:18 -0300

    pegando o input pelo id e adicionando a função task

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // ADICIONAR TAREFA NA LISTA
 const buttonAddTask = document.getElementById('criar-tarefa');
+const input = document.getElementById('texto-tarefa');
+
+const addTask = () => {
+  const valueInput = input.value
+  createListItem(valueInput)
+  clearInput()
+} 
 
 buttonAddTask.addEventListener('click', addTask)
