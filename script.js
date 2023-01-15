2eb720eb632d48319d2a60fa13b3a40bf31efc0d
Willian <willian.amorim1@outlook.com>
2023-01-14 22:01:21 -0300

    adicionado atributo de onClick nas li e criado função para adicionar classe de cor cinza caso clicado

--- script.js
+++ script.js
@@ -3,14 +3,30 @@ const buttonAddTask = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const sortedList = document.getElementById('lista-tarefas');
 
+const selectedItem = (event) => {
+  const li = document.querySelectorAll('li')
+  const itemClicked = event.target
+
+  if(!itemClicked.classList.contains('selected')) {
+    for (let i = 0; i < li.length; i++) {
+      if (li[i].classList.contains('selected')) {
+        li[i].classList.remove('selected');
+      }
+    }
+  }
+
+  itemClicked.classList.add('selected')
+}
+
 const clearInput = () => {
   input.value = ''
 }
 
 const createListItem = (valueInput) => {
-  const createLi = document.createElement('li')
-  sortedList.appendChild(createLi)
-  createLi.innerHTML = valueInput
+  const createLi = document.createElement('li');
+  createLi.setAttribute('onClick', 'selectedItem(event)');
+  sortedList.appendChild(createLi);
+  createLi.innerHTML = valueInput;
 }
 
 const addTask = () => {
@@ -20,3 +36,4 @@ const addTask = () => {
 } 
 
 buttonAddTask.addEventListener('click', addTask)
+
